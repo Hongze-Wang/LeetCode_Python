f9bf96dd1de02e86ac741db52542d1b539fa841d
Steve Wang <wanghongzecn@qq.com>
2019-10-04 12:02:44 +0800

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,10 @@
+# 27. Remove Element
+
+class Solution:
+    def removeElement(self, nums: List[int], val:int) -> int:
+        while True:
+            try:
+                nums.remove(val)
+            except:
+                break
+        return len(nums)
