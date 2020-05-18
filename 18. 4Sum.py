93a4c678677ed5a2cc55e45b20556f2e90b745e2
Steve Wang <wanghongzecn@qq.com>
2020-05-18 17:40:02 +0800

    Create 18. 4Sum.py

---
+++ 18. 4Sum.py
@@ -0,0 +1,18 @@
+# 18. 4Sum
+# turn into a two sum question and solve it
+from collections import defaultdict
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = set()
+        seen = defaultdict(list)
+
+        for i in range(len(nums)):
+            for j in range(i):
+                have = nums[i] + nums[j]
+                need = target - have
+                if need in seen:
+                    for k,t in seen[need]:
+                        if len({k,t,i,j}) == 4:
+                            res.add(tuple(sorted([nums[k], nums[t], nums[i], nums[j]])))
+                seen[have].append((i ,j))  # avoid collision & this why we should judge the len before add into res
+        return res
