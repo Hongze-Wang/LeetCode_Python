b06d655df75c3e0bff7c9cb9087744bcd4b7f723
Steve Wang <wanghongzecn@qq.com>
2020-06-06 17:22:31 +0800

    Create 75. Sort Colors.py

---
+++ 75. Sort Colors.py
@@ -0,0 +1,17 @@
+# 75. Sort Colors
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low = curr = 0
+        high = len(nums) - 1
+
+        while curr <= high:
+            if nums[curr] == 0:
+                nums[low], nums[curr] = nums[curr], nums[low]
+                low += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[high] = nums[high], nums[curr]
+                high -= 1
+            else:
+                curr += 1
