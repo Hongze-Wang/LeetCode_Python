9067634b877e32774c6a356124e6b54d07877e13
Steve Wang <wanghongzecn@qq.com>
2020-07-18 14:55:03 +0800

    Update 21. Merge Two Sorted Lists.py

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -5,6 +5,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         head = ListNode(-1)
@@ -24,4 +25,22 @@ class Solution:
         else:
             rear.next = l1
 
+        return head.next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = head = ListNode(-1);
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+            
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
         return head.next
