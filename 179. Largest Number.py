d535d4468a674f99452199de84d76faca2c0dfeb
Steve Wang <wanghongzecn@qq.com>
2020-07-29 21:47:15 +0800

    Create 179. Largest Number.py

---
+++ 179. Largest Number.py
@@ -0,0 +1,10 @@
+# 179. Largest Number
+
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+    
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        res = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if res[0] == '0' else res
