8983b28df18572c40b824c49f1700303f58ca0e1
Steve Wang <wanghongzecn@qq.com>
2020-07-31 16:12:30 +0800

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -9,3 +9,24 @@ class Solution:
             elif not stack or match[stack.pop()] != c:
                 return False
         return not stack
+
+# class Solution(object):
+#     def isValid(self, s):
+#         while "()" in s or "{}" in s or '[]' in s:
+#             s = s.replace("()", "").replace('{}', "").replace('[]', "")
+#         return s == ''
+
+# class Solution:
+#     def isValid(self, s):
+#         bracketMap = {"(": ")", "[": "]", "{": "}"}
+#         # openBracket = set(["(", "[", "{"])
+#         openBracket = ["(", "[", "{"]
+#         stack = []
+#         for i in s:
+#             if i in openBracket:
+#                 stack.append(i)
+#             elif stack and i == bracketMap[stack[-1]]:
+#                 stack.pop()
+#             else:
+#                 return False
+#         return stack == []
