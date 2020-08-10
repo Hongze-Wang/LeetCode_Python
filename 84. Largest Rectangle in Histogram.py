cb8139d0a3b7dd070254bb96feb6c1663c3db49a
Steve Wang <wanghongzecn@qq.com>
2020-08-10 10:02:35 +0800

    Create 84. Largest Rectangle in Histogram.py

---
+++ 84. Largest Rectangle in Histogram.py
@@ -0,0 +1,28 @@
+# 84. Largest Rectangle in Histogram
+# 递增栈解法基于一个假设 最大面积一定在一个递增序列上得到
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = [-1]
+        res = 0
+        heights.append(-1)
+
+        for idx, val in enumerate(heights):
+            while heights[stack[-1]] > val:
+                h = heights[stack.pop()]
+                res = max(res, h*(idx - stack[-1]-1))
+            stack.append(idx)
+        return res
+
+# Time Limit Exceeded
+# class Solution:
+#     def largestRectangleArea(self, heights: List[int]) -> int:
+#         if not heights:
+#             return 0
+#         res = 0
+#         for i in range(len(heights)):
+#             min_h = heights[i];
+#             for j in range(i, len(heights)):
+#                 min_h = min(min_h, heights[j])
+#                 res = max(res, min_h * (j-i+1))
+#         return res
