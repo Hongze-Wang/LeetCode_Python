ab0b84417ff0c0cbd7b5fe27698c40f559fc731a
Steve Wang <wanghongzecn@qq.com>
2020-08-17 21:06:01 +0800

    Update 55. Jump Game.py

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -1,8 +1,40 @@
 # 55. Jump Game
+
+# simple backtracking: Time Limit Exceed
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         def canReach(nums, index):
+#             if index ==a n-1:
+#                 return True
+#             fur = min(index+nums[index], n-1)
+#             for i in range(index+1, fur+1):
+#                 if canReach(nums, i):
+#                     return True
+#             return False
+                
+#         return canReach(nums, 0)
+
+# optimized Time Limit Exceed
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         def canReach(nums, index):
+#             if index == n-1:
+#                 return True
+#             fur = min(index+nums[index], n-1)
+#             for i in range(fur, index, -1):
+#                 if canReach(nums, i):
+#                     return True
+#             return False
+                
+#         return canReach(nums, 0)
+
+# Greedy
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        lastPos = len(nums) - 1
+        lastPos = len(nums)-1
         for i in range(lastPos, -1, -1):
-            if i+nums[i] >= lastPos:
+            if i + nums[i] >= lastPos:
                 lastPos = i
         return lastPos == 0
