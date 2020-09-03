6282ede68ba230d343dff3889f4f9732472254ea
Steve Wang <wanghongzecn@qq.com>
2020-09-03 10:07:47 +0800

    Create 322. Coin Change.py

---
+++ 322. Coin Change.py
@@ -0,0 +1,13 @@
+# 322. Coin Change
+
+# 找零问题 DP
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for j in range(len(coins)):
+                if i >= coins[j]:
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1)
+        return dp[amount] if dp[amount] < amount+1 else -1
