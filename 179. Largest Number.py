7cef18f65c66235a42c26d59afe22e4d4f80c7a1
Steve Wang <wanghongzecn@qq.com>
2020-12-25 16:22:10 +0800

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -1,10 +1,21 @@
 # 179. Largest Number
 
-class LargerNumKey(str):
-    def __lt__(x, y):
-        return x+y > y+x
-    
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        res = ''.join(sorted(map(str, nums), key=LargerNumKey))
-        return '0' if res[0] == '0' else res
+        lis = []
+        for i in range(len(nums)):
+            lis.append(str(nums[i]))
+        
+        lis = sorted(lis, key=cmp_to_key(lambda a, b: 1 if b+a > a+b else -1))
+        
+        res = "".join(lis)
+        return "0" if res[0] == "0" else res
+
+class Cmp(str):
+    def __lt__(x, y): # 函数名不可更改
+        return x+y > y+x
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        res = "".join(sorted(map(str, nums), key=Cmp))
+        return "0" if res[0] == "0" else res
