d427efefe6ff7734af64e71c8c4cef8eb81fb44f
Steve Wang <wanghongzecn@qq.com>
2021-01-06 21:32:19 +0800

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,5 +1,6 @@
 # 70. Climbing Stairs
 
+# math
 # class Solution:
 #     def climbStairs(self, n):
         
@@ -17,6 +18,7 @@
 #             nums.append(nums[i-1] + nums[i-2])
             
 #         return nums[n-1]
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         d = dict()
@@ -25,3 +27,19 @@ class Solution:
         for i in range(3, n+1):
             d[i] = d[i-1] + d[i-2]
         return d[n]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        
+        dp = [0] * (n+1)
+        
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[n] # dp[-1]
