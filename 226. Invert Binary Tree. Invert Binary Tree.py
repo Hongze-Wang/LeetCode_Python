95d95a0c2bcbe55b19ce12c232166c1d330d242a
Steve Wang <wanghongzecn@qq.com>
2021-01-14 15:58:02 +0800

    new files

---
+++ 226. Invert Binary Tree. Invert Binary Tree.py
@@ -0,0 +1,23 @@
+# 更多解法 参见剑指 Offer 27. 二叉树的镜像.java
+
+# 226. Invert Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
