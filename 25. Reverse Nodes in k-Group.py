12e1b5afab2ada2c099814bc24a4343ff947dfe9
Steve Wang <wanghongzecn@qq.com>
2021-11-30 00:06:00 +0800

    Update 25. Reverse Nodes in k-Group.py

--- 25. Reverse Nodes in k-Group.py
+++ 25. Reverse Nodes in k-Group.py
@@ -32,5 +32,35 @@ class Solution:
                 head.next = nex
             else:
                 head.next = self.reverseKGroup(nex, k)
-                
+
+# optimized
+class Solution:
+    def getLength(self, head):
+        length = 0
+        
+        while head:
+            length += 1
+            head = head.next
+            
+        return length
+    
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or k < 0:
+            return
+        
+        pre, cur, nex = None, None, head
+        count = 0
+        while nex and count < k:
+            pre = cur
+            cur = nex
+            nex = cur.next
+            cur.next = pre
+            count += 1
+    
+        if k > self.getLength(nex):
+            head.next = nex
+        else:
+            head.next = self.reverseKGroup(nex, k)
+            
+        return cur
         return cur
